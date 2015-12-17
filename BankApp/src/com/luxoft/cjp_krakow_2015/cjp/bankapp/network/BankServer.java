b1a430b8d9ea28941b963eadbc771317b433ec23
Prosner <Prosner@PL-CJP-02.poland.luxoft.com>
2015-12-17 11:16:56 +0100

    Added handleRequest() method

--- BankApp/src/com/luxoft/cjp_krakow_2015/cjp/bankapp/network/BankServer.java
+++ BankApp/src/com/luxoft/cjp_krakow_2015/cjp/bankapp/network/BankServer.java
@@ -5,8 +5,16 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
 
+import com.luxoft.cjp_krakow_2015.cjp.bankapp.models.Account;
 import com.luxoft.cjp_krakow_2015.cjp.bankapp.models.Bank;
+import com.luxoft.cjp_krakow_2015.cjp.bankapp.models.Client;
+import com.luxoft.cjp_krakow_2015.cjp.bankapp.models.exceptions.BankException;
+import com.luxoft.cjp_krakow_2015.cjp.bankapp.network.commands.LoginCmd;
+import com.luxoft.cjp_krakow_2015.cjp.bankapp.network.commands.MyAccountsCmd;
+import com.luxoft.cjp_krakow_2015.cjp.bankapp.network.commands.NetCommand;
+import com.luxoft.cjp_krakow_2015.cjp.bankapp.network.commands.WithdrawCmd;
 import com.luxoft.cjp_krakow_2015.cjp.bankapp.service.BankCommander;
 
 public class BankServer {
@@ -16,8 +24,14 @@ public class BankServer {
 	private ObjectOutputStream out;
 	private ObjectInputStream in;
 	private String message;
+	private NetCommand command;
 	
 	private Bank activeBank = BankCommander.bank;
+	private Client loggedClient;
+	
+	public BankServer(Bank bank) {
+		activeBank = bank;
+	}
 	
 	public void run() {
 		try {
@@ -33,8 +47,10 @@ public class BankServer {
 			do {
 				try {
 					
-					message = (String) in.readObject();
-					System.out.println("client>> " + message);
+					command = (NetCommand) in.readObject();
+					sendMessage(handleRequest(command));
+					
+					
 					if(message.equals("bye")) {
 						sendMessage("bye");
 					}
@@ -58,6 +74,37 @@ public class BankServer {
 			}
 		}
 	}
+	
+	private String handleRequest(NetCommand command) {
+		if(command.getClass() == LoginCmd.class) {
+			if(activeBank.getClient(((LoginCmd) command).getLogin()) != null) {
+				loggedClient = activeBank.getClient(((LoginCmd) command).getLogin());
+				return "Logged in";
+			}
+			else {
+				return "Username incorrect";
+			}
+		}
+		else if(command.getClass() == MyAccountsCmd.class) {
+			StringBuilder response = new StringBuilder();
+			List<Account> accounts = loggedClient.getAccountsList();
+			for(Account account : accounts) {
+				response.append(account.toString()).append("\\n");
+			}
+			return response.toString();
+			
+		}
+		else if(command.getClass() == WithdrawCmd.class) {
+			try {
+				loggedClient.withdraw(((WithdrawCmd)command).getAmount());
+			} catch (BankException e) {
+				return e.getMessage();
+			}
+			return "Withdrawn: " + ((WithdrawCmd)command).getAmount();
+		}
+		
+		return null;
+	}
 
 	private void sendMessage(final String msg) {
 		try {
@@ -74,7 +121,7 @@ public class BankServer {
 	}
 	
 	public static void main(String[] args) {
-		BankServer server = new BankServer();
+		BankServer server = new BankServer(new Bank());
 		server.run();
 	}
 	
