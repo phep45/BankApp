98b2e8c14201d7bdedbb9abab32c8a55079974fe
Prosner <Prosner@PL-CJP-02.poland.luxoft.com>
2016-01-11 15:30:30 +0100

    LoginServlet added

---
+++ BankApp/src/main/java/com/luxoft/cjp_krakow_2015/cjp/bankapp/servlets/LoginServlet.java
@@ -0,0 +1,43 @@
+package com.luxoft.cjp_krakow_2015.cjp.bankapp.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.logging.Logger;
+
+public class LoginServlet extends HttpServlet {
+
+	Logger log = Logger.getLogger("clients." + this.getClass().getName());
+	
+	private static final long serialVersionUID = 5308814134579210996L;
+	
+	protected void doPost(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+		final String name = request.getParameter("name");
+		final String email = request.getParameter("email");
+		final String pass = request.getParameter("pass");
+		
+		if(name == null) {
+			log.fine("Client not found");
+			throw new ServletException("No client specified");
+		}
+		request.getSession().setAttribute("name", name);
+		log.info("Client " + name + " logged into ATM");
+		if(isSuperuser(name, email, pass)) {
+			response.sendRedirect("/BankStats.html");
+		}
+		else 
+			response.sendRedirect("/ATM.html");
+	}
+
+	private boolean isSuperuser(String name, String email, String pass) {
+		if(!"superuser".equals(name)) return false;
+		if(!"super@user.com".equals(email)) return false;
+		if(!"super".equals(pass)) return false;
+		
+		return true;
+	}
+}
