208076246378ba48d36034637b9c75b81c6b3263
PRosner <PRosner@PL-CJP-02.poland.luxoft.com>
2016-01-12 13:16:24 +0100

    Session listener fixed

--- BankApp/src/main/java/com/luxoft/cjp_krakow_2015/cjp/bankapp/servlets/SessionListener.java
+++ BankApp/src/main/java/com/luxoft/cjp_krakow_2015/cjp/bankapp/servlets/SessionListener.java
@@ -1,21 +1,29 @@
 package com.luxoft.cjp_krakow_2015.cjp.bankapp.servlets;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
 public class SessionListener implements HttpSessionListener {
 
+	static Integer clientsConnected;
+	
+	Logger log = Logger.getLogger("clients." + this.getClass().getName());
+	
 	@Override
 	public void sessionCreated(HttpSessionEvent httpSessionEvent) {
 		final ServletContext context = httpSessionEvent.getSession().getServletContext();
 		synchronized(SessionListener.class) {
-			Integer clientsConnected = (Integer) context.getAttribute("clientsConnected");
+			clientsConnected = (Integer) context.getAttribute("clientsConnected");
 			if(clientsConnected == null) {
 				clientsConnected = 1;
 			}
 			else {
 				clientsConnected++;
+				log.log(Level.ALL, "client connected");
 			}
 			context.setAttribute("clientsConnected", clientsConnected);
 		}
@@ -25,12 +33,13 @@ public class SessionListener implements HttpSessionListener {
 	public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
 		final ServletContext context = httpSessionEvent.getSession().getServletContext();
 		synchronized (SessionListener.class) {
-			Integer clientsConnected = (Integer) context.getAttribute("clientsConnected");
+			clientsConnected = (Integer) context.getAttribute("clientsConnected");
 			if(clientsConnected == null) {
-				return;
+				clientsConnected = 1;
 			}
 			else {
 				clientsConnected--;
+				log.log(Level.ALL, "client disconnected");
 			}
 			context.setAttribute("clientsConnected", clientsConnected);
 		}
