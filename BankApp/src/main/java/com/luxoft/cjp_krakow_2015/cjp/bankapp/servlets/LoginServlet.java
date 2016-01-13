a31f022527a42bb672c07bff5bd377588e3a6131
Prosner <Prosner@PL-CJP-02.poland.luxoft.com>
2016-01-13 15:02:49 +0100

    reworked

--- BankApp/src/main/java/com/luxoft/cjp_krakow_2015/cjp/bankapp/servlets/LoginServlet.java
+++ BankApp/src/main/java/com/luxoft/cjp_krakow_2015/cjp/bankapp/servlets/LoginServlet.java
@@ -7,6 +7,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.luxoft.cjp_krakow_2015.cjp.bankapp.database.BankDAO;
+import com.luxoft.cjp_krakow_2015.cjp.bankapp.database.BankDAOImpl;
+import com.luxoft.cjp_krakow_2015.cjp.bankapp.database.BankNotFoundException;
+import com.luxoft.cjp_krakow_2015.cjp.bankapp.database.ClientDAO;
+import com.luxoft.cjp_krakow_2015.cjp.bankapp.database.ClientDAOImpl;
+import com.luxoft.cjp_krakow_2015.cjp.bankapp.database.DAOException;
+import com.luxoft.cjp_krakow_2015.cjp.bankapp.models.Bank;
+import com.luxoft.cjp_krakow_2015.cjp.bankapp.models.Client;
+
+import java.util.List;
 import java.util.logging.Logger;
 
 public class LoginServlet extends HttpServlet {
@@ -28,8 +38,24 @@ public class LoginServlet extends HttpServlet {
 		request.getSession().setAttribute("name", name);
 		log.info("Client " + name + " logged into ATM");
 		if(isSuperuser(name, email, pass)) {
-			response.sendRedirect("/BankStats.html");
-			log.info("Client redirected to /BankStats.html");
+			BankDAO bankDAO = new BankDAOImpl();
+			ClientDAO clientDAO = new ClientDAOImpl();
+
+			List<Client> clients = null;
+			Bank bank = null;
+			try {
+				bank = bankDAO.getBankByName("My Bank");
+				clients = clientDAO.getAllClients(bank);
+			} catch (DAOException e) {
+				e.printStackTrace();
+			} catch (BankNotFoundException e) {
+				e.printStackTrace();
+			}
+
+			request.setAttribute("clientsList", clients);
+			request.getRequestDispatcher("/BankStats.jsp").forward(request, response);
+			response.sendRedirect("/BankStats.jsp");
+			log.info("Client redirected to /BankStats.jsp");
 		}
 		else 
 			response.sendRedirect("/ATM.html");
